d4ec3f9d18e95affe57b1d394719db62eb7fe109
Mykyta <mykytamyrhorod@gmail.com>
2023-11-04 15:12:08 +0100

    aaa

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".header-top").classList.toggle("open")
     })
 })
+function fullVHfix() {
+    const fullScreens = document.querySelectorAll("[data-fullscreen]");
+    if (fullScreens.length && isMobile.any()) {
+      window.addEventListener("resize", fixHeight);
+      function fixHeight() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", ${vh}px);
+      }
+      fixHeight();
+    }
+  }
